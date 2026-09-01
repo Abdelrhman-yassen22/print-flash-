3a84219059412e57769f2142fdb2a001a24d183b
Abdelrhman-yassen22 <yasynbdalrhmnmhmd48@gmail.com>
2026-09-01 22:17:53 +0300

    Update model version from 1.5 to 2.5

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.set_page_config(
 try:
     api_key = st.secrets["GEMINI_API_KEY"]
     genai.configure(api_key=api_key)
-    model = genai.GenerativeModel('gemini-1.5-flash')
+  model = genai.GenerativeModel('gemini-2.5-flash')
     api_working = True
 except Exception as e:
     api_working = False
